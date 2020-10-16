e25ef4634eaa87014672292c5852db930ec1a21f
Ilja Semjonov <ilsemjonov@gmail.com>
2020-10-16 16:12:27 +0300

    move conditionals outside render()

--- src/index.js
+++ src/index.js
@@ -11,17 +11,25 @@ class App extends Component {
     window.navigator.geolocation.getCurrentPosition(
       (position) => this.setState({ lat: position.coords.latitude }),
       (err) => this.setState({ errorMessage: err.message })
-    )
+    );
   }
 
-  render() {
+  renderContent() { 
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>
     }
     if (!this.state.errorMessage && this.state.lat) {
       return <SeasonDisplay lat={this.state.lat} />
     }
-    return <Spinner message="Please accept location request"/>;
+    return <Spinner message="Please accept location request" />;
+  }
+
+  render() {
+    return (
+      <div className="border red">
+        {this.renderContent()}
+      </div>
+    );
   }
 }
 
