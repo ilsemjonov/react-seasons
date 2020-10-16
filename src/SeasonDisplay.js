574383f1360d493bdcc9f81924400ddc177493c7
Ilja Semjonov <ilsemjonov@gmail.com>
2020-10-16 15:06:11 +0300

    season styles

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,8 +1,9 @@
+import './SeasonDisplay.css'
 import React from 'react';
 
 const seasonConfig = {
     summer: {
-        text: 'Yeah, nice waves today',
+        text: 'Yeah, let\'s hit the beach',
         iconName: 'sun'
     },
     winter: {
@@ -24,10 +25,10 @@ const SeasonDisplay = (props) => {
     const { text, iconName } = seasonConfig[season];
 
     return (
-        <div>
-            <i className={`${iconName} icon`} />
+        <div className={`season-display ${season}`}>
+            <i className={`icon-left massive ${iconName} icon`} />
             <h1>{text}</h1>
-            <i className={`${iconName} icon`} />
+            <i className={`icon-right massive ${iconName} icon`} />
         </div>
     );
 }
